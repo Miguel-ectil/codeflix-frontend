660ab2c67046bd8af535da1cc15da74b9ad5eb0c
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2023-11-23 22:32:51 -0300

    [ADD and UPD] Separando a navbar em pequenos conponentes facilitando a sua manutenção e organização

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,67 +1,18 @@
 'use client'
-import React, {useEffect, useState} from "react";
-import Image from 'next/image'
+import React from "react";
+import { UserProfile } from "./UserProfile";
+import { Logo } from "./Logo";
+import { NavLinks } from "./NavLinks";
+import { useScroll } from "../hooks/useScroll";
 
-const useScroll = () => {
-  const [isScrolled, setIsScrolled] = useState(false)
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  }, [])
-  
-  return isScrolled;
-};
-
-const Logo = () => (
-  <Image 
-    src='/logo.svg' 
-    alt='logo'
-    width={90} height={90}
-    className='cursor-pointer'
-  />
-)
-
-const NavLinks = () => (
-  <nav>
-    <ul className='hidden md:flex md:space-x-4'>
-    <li>Home</li>
-    <li>Tv Shows</li>
-    <li>Movies</li>
-    <li>Latest</li>
-    </ul>
-  </nav>
-)
-
-const UserProfile = () => (
-  <div className="flex items-center space-x-4">
-    <p className="hidden cursor-not-allowed lg:inline">Kids</p>
-    <Image 
-      src='/profile.png' 
-      alt='profile'
-      width={36} height={36}
-      className='rounded cursor-pointer'
-    />
-  </div>
-)
 export default function Header() {
   const isScrolled = useScroll();
 
   return (
     <header className={`
       ${isScrolled && 'bg-black'}
-      fixed top-0 z-50 flex w-full items-center justify-between p-2 transition-all lg:px-10 lg:py-6`}>
+      fixed top-0 z-50 flex w-full items-center justify-between p-2 transition-all lg:px-10 lg:py-6`}
+      >
       <div className="flex items-center space-x-2 md:space-x-8">
         <Logo />
         <NavLinks />
---
+++ src/components/NavLinks.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const NavLinks = () => (
+  <nav>
+    <ul className='hidden md:flex md:space-x-4'>
+      <li>Home</li>
+      <li>Tv Shows</li>
+      <li>Movies</li>
+      <li>Latest</li>
+    </ul>
+  </nav>
+);
