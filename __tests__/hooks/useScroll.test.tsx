2aab3dba9f7be2844f279eb0b3914713bea463de
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2023-11-23 23:36:00 -0300

    [ADD and UPD] Adicionando e instalando configurações de e alguns arquivos de teste, deixando pronto o priemiro teste do código

---
+++ __tests__/hooks/useScroll.test.tsx
@@ -0,0 +1,23 @@
+import { useScroll } from '@/hooks/useScroll';
+import { renderHook, act } from '@testing-library/react';
+
+describe('useScroll', () => {
+  it('should respond to scroll events', () => {
+    const { result } = renderHook(() => useScroll());
+
+    act(() => {
+      global.window.scrollY = 100;
+      global.window.dispatchEvent(new Event('scroll'));
+    });
+
+    expect(result.current).toBe(true);
+
+    act(() => {
+      global.window.scrollY = 0;
+      global.window.dispatchEvent(new Event('scroll'));
+    });
+
+    expect(result.current).toBe(false);
+
+  });
+});
