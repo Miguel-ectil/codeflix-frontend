848de87a1f6170c942b1866aba8632ca1faae0ab
Miguel-ectil <ectilmiguelmiguelectil@gmail.com>
2023-11-23 23:57:31 -0300

    [ADD] Adicionando essa funcionalidade do 'tailwind-scrollbar-hide' no layout global, para que seja aplicada a todas as telas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className='scrollbar-hide'>
+        {children}
+      </body>
     </html>
   )
 }
